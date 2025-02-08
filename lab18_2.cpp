49926bf55144993b4998de7b1bd32e00717d0b1c
GGTTGTGt <zz814170@gmail.com>
2025-02-08 17:35:45 +0700

    lab18_2 completed

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -5,8 +5,8 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect *R1, Rect *R2) {
+    double overlap_w = max(0.0, min(R1->x + R1->w, R2->x + R2->w) - max(R1->x, R2->x));
+    double overlap_h = max(0.0, min(R1->y, R2->y) - max(R1->y - R1->h, R2->y - R2->h));
+    return overlap_w * overlap_h;
 }
